20d22c0c65ab1a04b9288481f3ac7fea4bdf3fc6
hbdev62 <hbdev62@gmail.com>
2025-02-09 00:33:15 -0400

    reloading with a little change cause' seems like the deploy was bugged on cache

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,8 @@ import { BrowserRouter, Route, Routes } from 'react-router'
 import App from './App.tsx'
 import Layout from './components/layout/Layout.tsx'
 import NotFound from './components/NotFound.tsx'
+//import UserItem from './components/UserItem.tsx'
+
 import UserItem from './components/UserItem.tsx'
 import AuthLayout from './components/layout/AuthLayout.tsx'
 import Login from './components/login/Login.tsx'
